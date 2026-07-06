cda461528b326e096f17cd82eae1d723db82a284
muhammad ahmed <muhammadahmed251011@gmail.com>
2026-07-06 21:00:45 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,520 @@
+// Q1.Write a script to greet your website visitor using JS alert box.
+// alert("Welcome to my website")
+
+// Q2.Write a script to display following message on your web page:
+// alert("Error! Please enter a invalid password")
+
+// Q3. Write a script to display following message on your web page: (Hint : Use line break).
+// alert("Welcome to Js Land...\nHappy Coding!")
+
+// Q4. Write a script to display following messages in sequence:
+// alert("Welcome to Js Land")
+// alert("Happy Coding!")
+
+// Q6.Make use of alerts in your new/existing HTML & CSS project
+// alert("welcome to my website")
+
+
+
+// Chapter # 2:
+// Q1. Declare a variable called username.
+// let username = ("")
+
+// Q2.Declare a variable called myName & assign to it a string that represents your Full Name.
+// let myname = ("Muhammad Ahmed")
+
+// Q3.Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// let message = ("Hello World")
+// alert(message)
+
+// Q4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// let name  = ("Muhammad Ahmed")
+// alert(name)
+// let age  = ("15 Years Old")
+// alert(age)
+// let qualification  = ("Certified Mobile Application Develompent")
+// alert(qualification)
+
+// Q5. Write a script to display the following alert using one JS variable:
+// let fastfood = ("Pizza\nPizz\nPiz\nPi\nP")
+// alert(fastfood)
+
+// Q6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).
+// show the blow mentioned message in an alert box.(Hint: usestring concatenation)
+// let email = ("muhammadahmed251011@gmail.com")
+// alert("My email address is " + email)
+
+// Q7.  Declare a variable called book & give it the value “A  smarter way to learn JavaScript”. Display
+// the following message in an alert box:
+// let book = ("A smarter way to learn Javascript")
+// alert("I am trying to learn from the Book " + book)
+
+// Q9. Store following string in a variable and show in alert and browser through JS.
+// let pattern = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// alert(pattern)
+
+// Chapter # 3 : 
+// Q1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+//  let age = ("I am 15 Years Old")
+// alert(age)
+
+// Q2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. 
+// Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+// let visits = 14 
+// alert("You have visited this site " + visits + " time")
+
+// Q3. Declare a variable called birthYear & assign to it your birth year. Show the following
+//  message in your browser:
+// let birthyear = 2011
+// document.write("My birth year is " + birthyear + "<br>")
+// document.write(" data type of my declered variable is number")
+
+// Q4.A visitor visits an online clothing store www.xyzClothing.com .Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// let  visitorname = ("Muhammad Ahmed")
+// let  productname = ("T shirt")
+// let  quantity = 10
+// document.write(visitorname + " ordered " + quantity + " " + productname + "(s) on XYZ Clothing Store")
+
+// Chapter # 4 :
+// Q1. Declare 3 variables in one statement.
+// let age,product,name 
+
+// Q2. Declare 5 legal & 5 illegal variable names.
+// legal varriable:
+// let firstname
+// let _classsection
+// let age
+// let %percent
+// let grade
+// illegal varriable:
+// let 106lastname
+// let pre board
+// let pre-board
+// let function
+// let 10cars
+
+// Q3.Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,______ and ______.For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or_____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+// document.write("<h1> Rules for naming Js variables</h1>")
+// document.write("Variable names can only contain, $ and _.For example $my_1stVariable <br>")
+// document.write("Variable must begin with a letter, $ and _.For example $name, _name or name <br>")
+// document.write("Variable names are case sensitive <br>")
+// document.write("Varriable names should not be Js Keywords")
+
+// Chapter # 5
+// Q1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+// let num1 = 7
+// let num2 = 3
+// let answer = num1 + num2
+// document.write(" Sum of " + num1 + " and " + num2 + " is " + answer)
+
+// Q2.  Repeat task 1 for subtraction, multiplication, division & modulus.
+// let num1 = 3
+// let num2 = 2
+
+// let subtraction = num1 - num2
+// let Multiplication = num1 * num2
+// let Division = num1 / num2
+// let modulas = num1 % num2
+
+//  document.write(" Subtraction of " + num1 + " and " + num2 + " is " + subtraction + "<br>")
+//  document.write(" Multiplication of " + num1 + " and " + num2 + " is " + Multiplication + "<br>")
+//  document.write(" Division of " + num1 + " and " + num2 + " is " + Division + "<br>")
+//  document.write(" Modulas of " + num1 + " and " + num2 + " is " + modulas)
+
+
+// Q3.Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value by 3.
+// l. Output : “The remainder is : 0”.
+// let num
+// document.write("Value after variable declaration is " + num + "<br>")
+
+// num = 5
+// document.write(" intial Value: " + num + "<br>")
+
+// num++
+// document.write("Value after increment is " + num + "<br>")
+
+// num = num + 7
+// document.write("Value after addition is " + num + "<br>")
+
+// num--
+// document.write("Value after decrement is " + num + "<br>")
+
+// let remainder = num % 3
+// document.write("The remainder is " + remainder)
+
+// Q4. 4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 
+// tickets to a movie. Example output:
+// let ticketprice = 600
+// let tickets = 5
+// let cost = ticketprice * tickets
+// document.write("Total cost to buy 5 tickets to a movie is " + cost + "PKR")
+
+// Q5.Write a script to display multiplication table of any umber in your browser.
+// let num = 7
+// document.write("Table of " + num + "<br><br>")
+// document.write(num + "x1=" + num * 1 + "<br>")
+// document.write(num + "x2=" + num * 2 + "<br>")
+// document.write(num + "x3=" + num * 3 + "<br>")
+// document.write(num + "x4=" + num * 4 + "<br>")
+// document.write(num + "x5=" + num * 5 + "<br>")
+// document.write(num + "x6=" + num * 6 + "<br>")
+// document.write(num + "x7=" + num * 7 + "<br>")
+// document.write(num + "x8=" + num * 8 + "<br>")
+// document.write(num + "x9=" + num * 9 + "<br>")
+// document.write(num + "x10=" + num * 10)
+
+// Q6. The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here. 
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+// let celsius = 25 
+// let farenhiet = (celsius * 9 / 5) + 32
+// document.write(celsius + "°C is " + farenhiet + "°F" + "F<BR>")
+
+// let farenhiet2 = 70
+// let celsius2 = (farenhiet2 - 32) * 5/9
+// document.write(farenhiet2 + "°F is " + celsius2 + "°C")
+
+// Q7. Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store 
+// the following in variables
+// a. Price of item 1
+// b. Price of item 2
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2
+// e. Shipping charges
+// Compute the total cost & show the receipt in your browser.
+// let priceitem1 = 650
+// let priceitem2 = 100
+// let quantityitem1 = 3
+// let quantityitem2 = 7
+// let deliverycharges = 100
+// let totalbill = (priceitem1 * quantityitem1) + (priceitem2 * quantityitem2) + (deliverycharges)
+// document.write("<h1>Shopping Cart</h1>")
+// document.write("Price of item 1 is " + priceitem1 + "<br>")
+// document.write("Quantity of item 1 is " + quantityitem1 + "<br>")
+// document.write("price of item 2 is " + priceitem2 + "<br>")
+// document.write("Quantity of item 2 is " + quantityitem2 + "<br>")
+// document.write("Delivery Charges " + deliverycharges + "<br><br>")
+// document.write("Total cost of your order is: " + totalbill)
+
+// Chapter # 6-9:
+// Q1. Write a program to take a number in a variable, do the required arithmetic to display the following result in your browser:
+// let a = 10 
+
+// document.write("Result:<br><br>")
+// document.write("The value of a is: " + a + "<br><br>")
+// document.write("-------------------------------------------<br><br>")
+
+// document.write("The value of ++a is: " + (++a) + "<br>")
+// document.write("Now the value of a is: " + a + "<br><br>")
+
+// document.write("The value of a++ is: " + (a++) + "<br>")
+// document.write("Now the value of a is: " + a + "<br><br>")
+
+// document.write("The value of --a is: " + (--a) + "<br>")
+// document.write("Now the value of a is: " + a + "<br><br>")
+
+// document.write("The value of a-- is: " + (a--) + "<br>")
+// document.write("Now the value of a is: " + a)
+
+
+// Q2. What will be the output in variables a, b & result after
+// execution of the following script:
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// Explain the output at each stage:
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--;
+// let a = 2 , b = 1
+// let result = --a - --b + ++b + b--
+// document.write("a is " + a + "<br>")
+// document.write("b is " + b + "<br>")
+// document.write("result is " + result)
+
+// Q3. Write a program that takes input a name from user & greet the user.
+// let name = prompt("Enter your name:")
+// alert("Hello, " + name + " Welcome! ")
+
+// Q4. Write a program to take input a number from user & display it’s multiplication table on your browser. If user
+// does not enter a new number, multiplication table of 5 should be displayed by default.
+// let num = prompt("Enter a number", "5")
+
+// if (num === "" || num === null) {
+//     num = 5
+// }
+
+// document.write("<h2>Table of " + num + "</h2>")
+
+//  document.write(num + " x 1 = " + num * 1 + "<br>")
+//  document.write(num + " x 2 = " + num * 2 + "<br>")
+//  document.write(num + " x 3 = " + num * 3 + "<br>")
+//  document.write(num + " x 4 = " + num * 4 + "<br>")
+//  document.write(num + " x 5 = " + num * 5 + "<br>")
+//  document.write(num + " x 6 = " + num * 6 + "<br>")
+//  document.write(num + " x 7 = " + num * 7 + "<br>")
+// document.write(num + " x 8 = " + num * 8 + "<br>")
+// document.write(num + " x 9 = " + num * 9 + "<br>")
+//  document.write(num + " x 10 = " + num * 10 + "<br>")
+
+// Chapter # 9-11 :
+// Q1. Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this: 
+// “Welcome to city of lights”
+// let city = prompt("Enter your city name:")
+// if (city.toLowerCase() == "karachi") {
+//     alert("Welcome to city of lights")
+// }
+
+// Q2. Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the 
+// user is female, give the message: Good Morning Ma’am.
+// let gender = prompt("Enter your gender:")
+// if (gender.toLowerCase() == "male") {
+//     alert("Good Morning Sir")
+// }
+// else if(gender.toLowerCase() == "female"){
+//     alert("Good Morning Ma'am")
+// }
+// else {
+//     alert("invalid gender")
+// }
+
+// Q3. Write a program to take input color of road traffic signal from the user & show the message according to this table:
+// let colour = prompt("Enter your traffic signal colour:")
+// if (colour.toLowerCase() == "red") {
+//     alert("Must Stop")
+// }
+// else if(colour.toLowerCase() == "yellow"){
+//     alert("Ready to move")
+// }
+// else if(colour.toLowerCase() == "green"){
+//     alert("Move now")
+// }
+// else {
+//     alert("invalid colour")
+// }
+
+
+
+
+// Write a program to check whether the given number is divisible by 3. Show the message to the user if the number is divisible by 3.
+// let num = +prompt("Enter a number:")
+// if (num % 3 == 0) {
+//     alert("The number is divisible by 3")
+// }
+// else {
+//     alert("The number is not divisible by 3")
+// }
+
+// Write a program that checks whether the given input is an even number or an odd number.
+// let num = +prompt("Enter a number:")
+// if (num % 2 == 0) {
+//     alert("Even Number")
+// }
+// else {
+//     alert("odd number")
+// }
+
+// 10. Write a program that takes temperature as input and
+// shows a message based on following criteria
+// a. T > 40 then “It is too hot outside.”
+// b. T > 30 then “The Weather today is Normal.”
+// c. T > 20 then “Today’s Weather is cool.”
+// d. T > 10 then “OMG! Today’s weather is so Cool.”
+// let temperature = +prompt("Enter correct temperature")
+// if (temperature > 40) {
+//     alert("It is to hot outside")
+// }
+// else if(temperature > 30){
+//     alert("The weather of today is normal")
+// }
+// else if(temperature > 20){
+//     alert("Today's weather is cool")
+// }
+// else if(temperature > 10){
+//     alert(" OMG! Today's weather  is so cool")
+// }
+// else{
+//     alert("Very cold weather")
+// }
+
+// Chapter # 12-13
+//  Write a JavaScript program that accept two integers and display the larger. Also show if the two integers are equal.
+// let num1 = +prompt("Enter your first number:")
+// let num2 = +prompt("Enter your second number:")
+// if (num1 > num2) {
+//     alert("Larger number is " + num1)
+// }
+// else if(num2 > num1){
+//     alert("Larger number is " + num2)
+// }
+// else{
+//     alert("Both are equaly")
+// }
+
+// Write a program that takes input a number from user & state whether the number is positive, negative or zero.
+// let num = +prompt("Enter a number:")
+// if (num > 0) {
+//     alert("Positive Number")
+// }
+// else if(num < 0){
+//     alert("Negative Number")
+// }
+// else{
+//     alert("Zero")
+// }
+
+// 5. Write a program that
+// a. Store correct password in a JS variable.
+// b. Asks user to enter his/her password
+// c. Validate the two passwords:
+// i. Check if user has entered password. If not, then
+// give message “ Please enter your password”
+// ii. Check if both passwords are same. If they are
+// same, show message “Correct! The password you
+// entered matches the original password”. Show
+// “Incorrect password” otherwise.
+let correctpassword = ("best1234")
+let userpassword = prompt("Enter your password")
+if (userpassword === "") {
+    alert("Please! enter your password")
+}
+else if(userpassword === correctpassword){
+    alert("Correct! The password you enterd is same as the original one")
+}
+else{
+    alert("Password is incorrect")
+}
+
+// Q6. This if/else statement does not work. Try to fix it:
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// else
+// greeting = "Good evening";
+// }
+var greeting;
+var hour = 13;
+if (hour < 18) {
+greeting = "Good day"
+}
+else{
+    greeting = "Good evening";
+}
+alert(greeting)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
